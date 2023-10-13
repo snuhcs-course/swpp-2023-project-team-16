6f3cd337d6b010f642f16b32e8b35eb2e86772b8
Jonghyun Lee <lukeqwaszx@gmail.com>
2023-10-13 13:47:27 +0900

    feat: modify .gitignore and add material3

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
