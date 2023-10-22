cd487d2d0af01a9309ed50b697b6029eb8d59aff
seonu4949@snu.ac.kr <seonu4949@snu.ac.kr>
2023-10-22 15:18:41 +0900

    rename : PEP8 형식으로 코드 수정

--- Object dection using image/yolo.py
+++ Object dection using image/yolo.py
@@ -1,120 +1,121 @@
 # USAGE
 # python yolo.py --image images/baggage_claim.jpg
-# import the necessary packages
-# copy from github yolov5
+# Import the necessary packages
+# Copyright from GitHub YOLOv5
+
 import numpy as np
 import argparse
 import time
 import cv2
 import os
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3,
-	help="threshold when applyong non-maxima suppression")
-args = vars(ap.parse_args())
+# Construct the argument parser and parse the arguments
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("-i", "--image", required=True,
+                help="path to input image")
+arg_parser.add_argument("-c", "--confidence", type=float, default=0.5,
+                help="minimum probability to filter weak detections")
+arg_parser.add_argument("-t", "--threshold", type=float, default=0.3,
+                help="threshold when applying non-maxima suppression")
+args = vars(arg_parser.parse_args())
 
-# load the COCO class labels our YOLO model was trained on
-# labelsPath = 'yolo-coco\coco.names'
-labelsPath = '../yolo-coco/coco.names'
-LABELS = open(labelsPath).read().strip().split("\n")
+# Load the COCO class labels our YOLO model was trained on
+# labels_path = 'yolo-coco/coco.names'
+labels_path = '../yolo-coco/coco.names'
+LABELS = open(labels_path).read().strip().split("\n")
 
-# initialize a list of colors to represent each possible class label
+# Initialize a list of colors to represent each possible class label
 np.random.seed(42)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
-	dtype="uint8")
+                           dtype="uint8")
 
-# derive the paths to the YOLO weights and model configuration
-weightsPath = 'yolo-coco\yolov3.weights'
-configPath = 'yolo-coco\yolov3.cfg'
+# Derive the paths to the YOLO weights and model configuration
+weights_path = 'yolo-coco/yolov3.weights'
+config_path = 'yolo-coco/yolov3.cfg'
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
-print("[INFO] loading YOLO from disk...")
-net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+# Load our YOLO object detector trained on the COCO dataset (80 classes)
+print("[INFO] Loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
 
-# load our input image and grab its spatial dimensions
+# Load our input image and grab its spatial dimensions
 image = cv2.imread(args["image"])
 (H, W) = image.shape[:2]
 
-# determine only the *output* layer names that we need from YOLO
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+# Determine only the *output* layer names that we need from YOLO
+layer_names = net.getLayerNames()
+output_layer_names = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-# construct a blob from the input image and then perform a forward
+# Construct a blob from the input image and then perform a forward
 # pass of the YOLO object detector, giving us our bounding boxes and
 # associated probabilities
 blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
-	swapRB=True, crop=False)
+                            swapRB=True, crop=False)
 net.setInput(blob)
-start = time.time()
-layerOutputs = net.forward(ln)
-# [,frame,no of detections,[classid,class score,conf,x,y,h,w]
-end = time.time()
+start_time = time.time()
+layer_outputs = net.forward(output_layer_names)
+# [, frame, no_of_detections, [class_id, class_score, conf, x, y, h, w]
+end_time = time.time()
 
-# show timing information on YOLO
-print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+# Show timing information on YOLO
+print("[INFO] YOLO took {:.6f} seconds".format(end_time - start_time))
 
-# initialize our lists of detected bounding boxes, confidences, and
+# Initialize our lists of detected bounding boxes, confidences, and
 # class IDs, respectively
-boxes = []
+bounding_boxes = []
 confidences = []
-classIDs = []
+class_ids = []
 
-# loop over each of the layer outputs
-for output in layerOutputs:
-	# loop over each of the detections
-	for detection in output:
-		# extract the class ID and confidence (i.e., probability) of
-		# the current object detection
-		scores = detection[5:]
-		classID = np.argmax(scores)
-		confidence = scores[classID]
+# Loop over each of the layer outputs
+for output in layer_outputs:
+    # Loop over each of the detections
+    for detection in output:
+        # Extract the class ID and confidence (i.e., probability) of
+        # the current object detection
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
 
-		# filter out weak predictions by ensuring the detected
-		# probability is greater than the minimum probability
-		if confidence > args["confidence"]:
-			# scale the bounding box coordinates back relative to the
-			# size of the image, keeping in mind that YOLO actually
-			# returns the center (x, y)-coordinates of the bounding
-			# box followed by the boxes' width and height
-			box = detection[0:4] * np.array([W, H, W, H])
-			(centerX, centerY, width, height) = box.astype("int")
+        # Filter out weak predictions by ensuring the detected
+        # probability is greater than the minimum probability
+        if confidence > args["confidence"]:
+            # Scale the bounding box coordinates back relative to the
+            # size of the image, keeping in mind that YOLO actually
+            # returns the center (x, y)-coordinates of the bounding
+            # box followed by the box's width and height
+            box = detection[0:4] * np.array([W, H, W, H])
+            (center_x, center_y, width, height) = box.astype("int")
 
-			# use the center (x, y)-coordinates to derive the top and
-			# and left corner of the bounding box
-			x = int(centerX - (width / 2))
-			y = int(centerY - (height / 2))
+            # Use the center (x, y)-coordinates to derive the top and
+            # left corner of the bounding box
+            x = int(center_x - (width / 2))
+            y = int(center_y - (height / 2))
 
-			# update our list of bounding box coordinates, confidences,
-			# and class IDs
-			boxes.append([x, y, int(width), int(height)])
-			confidences.append(float(confidence))
-			classIDs.append(classID)
+            # Update our list of bounding box coordinates, confidences,
+            # and class IDs
+            bounding_boxes.append([x, y, int(width), int(height)])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
 
-# apply non-maxima suppression to suppress weak, overlapping bounding
+# Apply non-maxima suppression to suppress weak, overlapping bounding
 # boxes
-idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
-	args["threshold"])
+indexes = cv2.dnn.NMSBoxes(bounding_boxes, confidences, args["confidence"],
+                       args["threshold"])
 
-# ensure at least one detection exists
-if len(idxs) > 0:
-	# loop over the indexes we are keeping
-	for i in idxs.flatten():
-		# extract the bounding box coordinates
-		(x, y) = (boxes[i][0], boxes[i][1])
-		(w, h) = (boxes[i][2], boxes[i][3])
+# Ensure at least one detection exists
+if len(indexes) > 0:
+    # Loop over the indexes we are keeping
+    for i in indexes.flatten():
+        # Extract the bounding box coordinates
+        (x, y) = (bounding_boxes[i][0], bounding_boxes[i][1])
+        (w, h) = (bounding_boxes[i][2], bounding_boxes[i][3])
 
-		# draw a bounding box rectangle and label on the image
-		color = [int(c) for c in COLORS[classIDs[i]]]
-		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-			0.5, color, 2)
+        # Draw a bounding box rectangle and label on the image
+        color = [int(c) for c in COLORS[class_ids[i]]]
+        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+        text = "{}: {:.4f}".format(LABELS[class_ids[i]], confidences[i])
+        cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, color, 2)
 
-# show the output image
+# Show the output image
 cv2.imshow("Image", image)
 cv2.waitKey(0)
