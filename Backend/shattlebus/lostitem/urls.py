2d613e3794e408c57de064bac16792e94c933949
ksiyeon27 <siyeonk27@gmail.com>
2023-10-27 20:19:54 +0900

    remove: delete lostitem app

--- Backend/shattlebus/lostitem/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('list/', views.LostItemView.as_view(), name='lostitem-list'),
-    path('', views.LostItemView.as_view(), name='lostitem-search'),
-]
