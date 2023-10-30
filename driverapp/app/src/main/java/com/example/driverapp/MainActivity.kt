a2fb9315cd2efaa5de89360393eaf9506c822d5f
Gamjae <dnjs3214@naver.com>
2023-10-31 03:13:15 +0900

    feat : Django 서버로 PUT /driverapp/update request 보내는 함수 구현

--- driverapp/app/src/main/java/com/example/driverapp/MainActivity.kt
+++ driverapp/app/src/main/java/com/example/driverapp/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import android.os.Handler
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
 import retrofit2.Call
@@ -16,13 +19,18 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
-
+private const val BASE_URL = "http://54.180.118.50:3000"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var startStopButton: Button
     private lateinit var gpsTracker: GPSTracker
+    private lateinit var licensePlateText: EditText
+    private lateinit var responseText: TextView
     private var isTracking: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +38,8 @@ class MainActivity : AppCompatActivity() {
 
         startStopButton = findViewById(R.id.startStopButton)
         startStopButton.setOnClickListener { toggleTracking() }
+        licensePlateText = findViewById(R.id.licensePlateText)
+        responseText = findViewById(R.id.responseText)
 
         // Request location permissions if not granted
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -60,13 +70,13 @@ class MainActivity : AppCompatActivity() {
             override fun run() {
                 if (isTracking) {
                     val location: Location? = gpsTracker.getLocation()
+                    val licensePlate: String = licensePlateText.text.toString()
                     if (location != null) {
                         // Construct a GPSData object
                         val gpsData = GPSData(
                             latitude = location.latitude,
                             longitude = location.longitude,
-                            created_at = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date()),
-                            updated_at = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date())
+                            licensePlate = licensePlate
                         )
 
                         // Send the GPSData object to the endpoint
@@ -80,33 +90,33 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-
-
-
     private fun sendDataToEndpoint(data: GPSData) {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://endpoint.com/") // Replace with your server's base URL
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val apiService = retrofit.create(ApiService::class.java)
-        val call = apiService.submitGPSData(data)
+        val call = apiService.submitGPSData(licensePlate = data.licensePlate,
+                                            latitude = data.latitude,
+                                            longitude = data.longitude)
 
         call.enqueue(object : Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 if (response.isSuccessful) {
                     // Handle a successful response from the server
+                    responseText.text = response.code().toString()
                 } else {
                     // Handle errors or unsuccessful responses
+                    responseText.text = response.code().toString()
                 }
             }
 
             override fun onFailure(call: Call<Void>, t: Throwable) {
                 // Handle network failures or request exceptions
+                responseText.text = t.message
             }
         })
     }
-
-
 }
 
