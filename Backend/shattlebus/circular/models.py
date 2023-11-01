6b2fde53b1ee3e440beac5fec7d65a2473130612
Gamjae <dnjs3214@naver.com>
2023-11-01 23:43:58 +0900

    refactor : Location 테이블의 type 필드 삭제

--- Backend/shattlebus/circular/models.py
+++ Backend/shattlebus/circular/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 
 class Location(models.Model):
-    type = models.PositiveIntegerField()
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     created_at = models.DateTimeField(auto_now_add=True)
