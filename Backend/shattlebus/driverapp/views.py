7c91ab5ec63a84936543aa55b9f63f5ec234acb2
Gamjae <dnjs3214@naver.com>
2023-11-01 23:46:21 +0900

    feat : PUT /driverapp/update API 구현

--- Backend/shattlebus/driverapp/views.py
+++ Backend/shattlebus/driverapp/views.py
@@ -1,5 +1,7 @@
 from django.views import View
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 import json
 
@@ -8,7 +10,6 @@ from circular.models import Location
 
 
 # Create your views here.
-
 
 class RetrieveCircularBusView(View):
 
@@ -30,3 +31,21 @@ class RetrieveCircularBusView(View):
                     }
 
         return HttpResponse(json.dumps(response))
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class UpdateCircularBusLocationView(View):
+
+    def put(self, request):
+        license_plate = request.GET['license_plate']
+        latitude = request.GET['latitude']
+        longitude = request.GET['longitude']
+
+        location_data = Location(latitude=latitude, longitude=longitude)
+        location_data.save()
+
+        CircularBus.objects\
+            .filter(license_plate=license_plate)\
+            .update(location=location_data, is_running=True, is_tracked=True)
+
+        return HttpResponse()
