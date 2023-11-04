a78c0595b585b1300a4f16706ff7f63069464351
ksiyeon27 <siyeonk27@gmail.com>
2023-11-04 10:20:08 +0900

    fix : updated_at_kr

--- Backend/shattlebus/dropoff/views.py
+++ Backend/shattlebus/dropoff/views.py
@@ -23,30 +23,31 @@ class RetrieveWaitingTimeView(View):
         current_line = CurrentLine.objects.all().values()[0]
         num_people_waiting = current_line['num_people_waiting']
         updated_at = current_line['updated_at']
+        updated_at_kr = updated_at + datetime.timedelta(hours=9)
 
         try:
             if not current_line['is_executing']:
                 raise NoShuttleException
 
-            waiting_data = self.get_waiting_data(num_people_waiting, updated_at)
+            waiting_data = self.get_waiting_data(num_people_waiting, updated_at_kr)
         except NoShuttleException:
             waiting_data = {
                 "num_waiting_people": num_people_waiting,
                 "waiting_time": -1,
                 "num_needed_bus": -1,
-                "updated_at": str(updated_at)
+                "updated_at": str(updated_at_kr)
             }
 
         return HttpResponse(json.dumps(waiting_data))
 
-    def get_waiting_data(self, num_people_waiting, updated_at):
+    def get_waiting_data(self, num_people_waiting, updated_at_kr):
         num_needed_bus = (num_people_waiting // MAX_NUM_OF_PEOPLE) + 1
         waiting_time = self.get_waiting_time(num_needed_bus)
         waiting_data = {
             "num_waiting_people": num_people_waiting,
             "waiting_time": waiting_time,
             "num_needed_bus": num_needed_bus,
-            "updated_at": str(updated_at)
+            "updated_at": str(updated_at_kr)
         }
 
         return waiting_data
