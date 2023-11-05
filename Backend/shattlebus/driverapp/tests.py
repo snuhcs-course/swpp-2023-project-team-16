ac7ab4711a4bc76f2db6e3546f573c989aee56a2
ksiyeon27 <siyeonk27@gmail.com>
2023-11-05 22:38:41 +0900

    test : Put /driverapp/update?license_plate=&longitude=&latitude=

--- Backend/shattlebus/driverapp/tests.py
+++ Backend/shattlebus/driverapp/tests.py
@@ -41,6 +41,7 @@ class RetrieveCircularBusTest(TestCase):
         print("\n- 내 순환 셔틀 버스 정보 얻기 success")
 
 class UpdateCircularBusLocationTest(TestCase):
+
     def setup(self):
         pass
 
@@ -49,4 +50,33 @@ class UpdateCircularBusLocationTest(TestCase):
         Location.objects.all().delete()
 
     def test_put_circular_bus_location(self):
-        pass
+        # Given
+        location = Location.objects.create(latitude=37.1111, longitude=126.1111)
+        location.save()
+        circular_bus = CircularBus.objects.create(
+            license_plate="16가1616", location_id=location.id, is_running=True, is_tracked=True
+        )
+        circular_bus.save()
+        original_updated_at_kr = location.updated_at+datetime.timedelta(hours=9)
+
+        # When
+        client = Client()
+        response = client.put(
+            '/driverapp/update?license_plate=16가1616&latitude=27.1111&longitude=136.1111',
+            secure=True
+        )
+        data = response.json()
+
+        # Then
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(CircularBus.objects.count(), 1)
+        self.assertEqual(Location.objects.count(), 1)
+        self.assertEqual(data["license_plate"], "16가1616")
+        self.assertEqual(data["latitude"], 27.1111)
+        self.assertEqual(data["longitude"], 136.1111)
+        self.assertEqual(data["is_running"], True)
+        self.assertEqual(data["is_tracked"], True)
+        self.assertNotEquals(data["location_updated_at"], str(original_updated_at_kr))
+
+        print("\n- 순환 셔틀 버스 location update success")
+
