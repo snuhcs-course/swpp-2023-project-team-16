011b2cf3a3acac50514a641b6c864091d4f64ea3
ksiyeon27 <siyeonk27@gmail.com>
2023-11-05 18:48:42 +0900

    test : Get /dropoff/waiting-time (executing)

--- Backend/shattlebus/dropoff/tests.py
+++ Backend/shattlebus/dropoff/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+from .models import CurrentLine
+
+class RetrieveWaitingTimeTest(TestCase):
+
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        CurrentLine.objects.filter(num_people_waiting=1616).delete()
+
+
+    def test_get_waiting_time_for_shuttle_when_executing(self):
+        # Given
+        current_line = CurrentLine.objects.create(num_people_waiting=1616, is_executing=True)
+        current_line.save()
+
+        # When
+        client = Client()
+        response = client.get('/dropoff/waiting-time', secure=True)
+        data = response.json()
+
+        # Then
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(CurrentLine.objects.count(), 1)
+        self.assertEqual(data["num_waiting_people"], CurrentLine.objects.all()[0].num_people_waiting)
+        self.assertEqual(data["num_waiting_people"], 1616)
+
+
+
+
+
