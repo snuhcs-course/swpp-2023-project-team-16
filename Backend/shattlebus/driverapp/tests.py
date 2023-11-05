9ac2d3e9adcbd63ba09d10e5a40284b77f18ee65
ksiyeon27 <siyeonk27@gmail.com>
2023-11-05 22:25:55 +0900

    test : Get /driverapp/circular/buses?license_plate=

--- Backend/shattlebus/driverapp/tests.py
+++ Backend/shattlebus/driverapp/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+from circular.models import CircularBus
+from circular.models import Location
+import datetime
+
+class RetrieveCircularBusTest(TestCase):
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        CircularBus.objects.all().delete()
+        Location.objects.all().delete()
+
+    def test_get_circular_bus(self):
+        # Given
+        location = Location.objects.create(latitude=37.1111, longitude=126.1111)
+        location.save()
+        circular_bus = CircularBus.objects.create(
+            license_plate="16가1616", location_id=location.id, is_running=True, is_tracked=True
+        )
+        circular_bus.save()
+
+
+        # When
+        client = Client()
+        response = client.get('/driverapp/circular/buses?license_plate=16가1616', secure=True)
+        data = response.json()
+
+        # Then
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(CircularBus.objects.count(), 1)
+        self.assertEqual(Location.objects.count(), 1)
+        self.assertEqual(data["license_plate"], "16가1616")
+        self.assertEqual(data["latitude"], 37.1111)
+        self.assertEqual(data["longitude"], 126.1111)
+        self.assertEqual(data["is_running"], True)
+        self.assertEqual(data["is_tracked"], True)
+        self.assertIsNotNone(data["location_updated_at"])
+
+        print("\n- 내 순환 셔틀 버스 정보 얻기 success")
+
+class UpdateCircularBusLocationTest(TestCase):
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        CircularBus.objects.all().delete()
+        Location.objects.all().delete()
+
+    def test_put_circular_bus_location(self):
+        pass
