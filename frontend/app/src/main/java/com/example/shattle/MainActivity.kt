71e8a79901c19e4d87f45bb564265f72420b4903
HyunjaeRoh <rohhj0410@gmail.com>
2023-11-07 15:09:27 +0900

    fix: info 열려있을 때 바텀 네비게이션 메뉴 비활성화

--- frontend/app/src/main/java/com/example/shattle/MainActivity.kt
+++ frontend/app/src/main/java/com/example/shattle/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.shattle
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.iterator
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
@@ -10,7 +12,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.shattle.databinding.ActivityMainBinding
 import com.example.shattle.ui.info.InfoDialog
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), InfoDialog.InfoDialogListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -29,10 +31,6 @@ class MainActivity : AppCompatActivity() {
 
         // Info 팝업 생성
         showInfo(supportFragmentManager)
-
-        // TODO Info 열려있을 때 버튼 비활성화하기
-        for (menu in navView.menu) menu.isEnabled = true
-
     }
 
     private fun showInfo(
@@ -44,19 +42,29 @@ class MainActivity : AppCompatActivity() {
             // 다른 팝업이 열려있는지 확인 (닫혀있다면 null 이 할당됨)
             val existingFragmentInfo = supportFragmentManager.findFragmentByTag("InfoFragment")
             // 다른 팝업이 닫혀 있는 경우에만 열기
-
-            if((existingFragmentInfo != null) ){
-
-            }
-            else {
+            if(existingFragmentInfo == null){
                 val infoDialog = InfoDialog()
                 val transaction = supportFragmentManager.beginTransaction()
                 // infoFragment 가 표시될 위치 설정 (infoContainer), tag 설정(tag 로 fragment 가 열려있는지 확인 가능)
                 transaction.add(R.id.infoContainer, infoDialog, "InfoFragment")
                 // transaction 은 생성 할 때마다 commit 해주기
                 transaction.commit()
+
+                // 네비게이션 버튼 비활성화
+                for (menu in binding.navView.menu)
+                    menu.isEnabled = false
+//                window.statusBarColor = ContextCompat.getColor(this, R.color.gray_transparent)
+//                binding.navView.setBackgroundColor(ContextCompat.getColor(this, R.color.gray_transparent))
             }
         }
+    }
+
+    override fun onDialogDismissed() {
+        // 네비게이션 버튼 활성화
+        for (menu in binding.navView.menu)
+            menu.isEnabled = true
+//        window.statusBarColor = ContextCompat.getColor(this, R.color.creme_white)
+//        binding.navView.setBackgroundColor(ContextCompat.getColor(this, R.color.creme_white))
     }
 
 }
