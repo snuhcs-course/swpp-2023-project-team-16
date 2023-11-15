1ba7fec78f3edbd4830b9ac1f83fc9de71192e21
HyunjaeRoh <rohhj0410@gmail.com>
2023-11-15 18:52:57 +0900

    design: dropoff 텍스트뷰 가시성 향상

--- frontend/app/src/main/java/com/example/shattle/ui/dropoff/DropoffUI.kt
+++ frontend/app/src/main/java/com/example/shattle/ui/dropoff/DropoffUI.kt
@@ -1,8 +1,11 @@
 package com.example.shattle.ui.dropoff
 
+import android.graphics.Color
 import android.graphics.Typeface
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
 import android.util.Log
 import android.widget.Button
@@ -10,6 +13,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import com.example.shattle.R
 import com.google.android.gms.maps.GoogleMap
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -32,49 +36,53 @@ class DropoffUI(
     }
 
     fun changeTextView(dropoffUIState: DropoffUIState) {
-        // 숫자 bold 적용
+        // 숫자 bold 적용, 크기 30sp 로 변경 (기본: 20sp)
 
-        val boldStyle = StyleSpan(Typeface.BOLD)
+        val numPeopleText = "현재 ${dropoffUIState.numPeople} 명 대기 중입니다."
+        val numBusText = "다음 ${dropoffUIState.numBus} 번째 버스 탑승 가능합니다."
+        val numTimeText = "예상 대기시간: ${dropoffUIState.numTime} 분"
 
-        val numPeopleText = "현재 ${dropoffUIState.numPeople}명 대기 중입니다."
-        val numBusText = "다음 ${dropoffUIState.numBus}번째 버스 탑승 가능합니다."
-        val numTimeText = "예상 대기시간: ${dropoffUIState.numTime}분"
+        tv_numPeople.text = applySpanAtNumber(numPeopleText, dropoffUIState.numPeople)
+        tv_numBus.text = applySpanAtNumber(numBusText, dropoffUIState.numBus)
+        tv_numTime.text = applySpanAtNumber(numTimeText, dropoffUIState.numTime)
+    }
 
-        val spannableNumPeople = SpannableString(numPeopleText)
-        val spannableNumBus = SpannableString(numBusText)
-        val spannableNumTime = SpannableString(numTimeText)
+    fun applySpanAtNumber(str: String, num: Int): SpannableString{
+        // 해당 문자열에서 숫자 부분만 스타일 변경 (bold, 크기, 색)
 
-        val startIndexPeople = numPeopleText.indexOf(dropoffUIState.numPeople.toString())
-        val endIndexPeople = startIndexPeople + dropoffUIState.numPeople.toString().length
+        val spannableString = SpannableString(str)
 
-        val startIndexBus = numBusText.indexOf(dropoffUIState.numBus.toString())
-        val endIndexBus = startIndexBus + dropoffUIState.numBus.toString().length
+        val startIndex = str.indexOf(num.toString())
+        val endIndex = startIndex + num.toString().length
 
-        val startIndexTime = numTimeText.indexOf(dropoffUIState.numTime.toString())
-        val endIndexTime = startIndexTime + dropoffUIState.numTime.toString().length
-
-        spannableNumPeople.setSpan(
+        // bold 로 변경
+        val boldStyle = StyleSpan(Typeface.BOLD_ITALIC)
+        spannableString.setSpan(
             boldStyle,
-            startIndexPeople,
-            endIndexPeople,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        spannableNumBus.setSpan(
-            boldStyle,
-            startIndexBus,
-            endIndexBus,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        spannableNumTime.setSpan(
-            boldStyle,
-            startIndexTime,
-            endIndexTime,
+            startIndex,
+            endIndex,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
 
-        tv_numPeople.text = spannableNumPeople
-        tv_numBus.text = spannableNumBus
-        tv_numTime.text = spannableNumTime
+        // 크기 변경
+        val sizeSpan = AbsoluteSizeSpan(30, true)
+        spannableString.setSpan(
+            sizeSpan,
+            startIndex,
+            endIndex,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        // 색 변경
+        val colorSpan = ForegroundColorSpan(Color.RED)
+        spannableString.setSpan(
+            colorSpan,
+            startIndex,
+            endIndex,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        return spannableString
     }
 
     fun changeVisualView(dropoffUIState: DropoffUIState) {
